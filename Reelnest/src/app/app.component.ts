28febef36e6aeca48b201bf00b95787fb8d07a52
Elisei-Ionel Morariu <elisei-ionel.morariu@continental-corporation.com>
2025-09-17 14:52:37 +0300

    header pos done, started contact form

---
+++ Reelnest/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BrandsComponent } from './brands/brands.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ServicesComponent } from './services/services.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    ServicesComponent,
+    BrandsComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Reelnest';
+}

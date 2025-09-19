91902fcfc5d20fb4dab640cd9f64b34d8bebced5
Elisei <eli10es@yahoo.es>
2025-09-19 17:48:54 +0300

    Added video in hero page, added contact button

--- Reelnest/src/app/header/header.component.ts
+++ Reelnest/src/app/header/header.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  scrollToContact() {
+    const footer = document.getElementById('contact');
+    footer?.scrollIntoView({ behavior: 'smooth' });
+  }
 }

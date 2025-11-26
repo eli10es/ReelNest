77552fbe6e20035a4988dd18150f504af9189ec2
Elisei-Ionel Morariu <elisei-ionel.morariu@continental-corporation.com>
2025-11-26 12:51:37 +0200

    Finished brand section, no slider on desktop screens

--- Reelnest/src/app/brands/brands.component.ts
+++ Reelnest/src/app/brands/brands.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-brands',
   imports: [],
   templateUrl: './brands.component.html',
-  styleUrl: './brands.component.css'
+  styleUrl: './brands.component.css',
 })
 export class BrandsComponent {
+  isDesktop = window.innerWidth >= 1024;
 
+  @HostListener('window:resize')
+  onResize() {
+    this.isDesktop = window.innerWidth >= 1024;
+  }
+
+  brands: string[] = [
+    'tutti.jpg',
+    'leaderImpact.jpg',
+    'baniiFamiliei.jpg',
+    'depUtilaje.jpg',
+    'aliniere.jpg',
+    'inspirium.jpg',
+    'baurent.jpg',
+  ];
 }
